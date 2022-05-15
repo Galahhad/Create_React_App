dc5e1a9f2d7e71b837b21b7b439641e263db5173
Galahad <steam_team_look@mail.ru>
2022-05-16 01:33:50 +0300

    Create App React

---
+++ first-cra-app/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./style.css";
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(
+  <h1 className="react_text">Create React App</h1>
+)
